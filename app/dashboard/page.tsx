9fb2d7e94127e97444d4556a4a06c5bd7d520564
Default462 <92624184+Default462@users.noreply.github.com>
2024-11-11 00:58:08 +0530

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,161 +1,146 @@
-import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { notFound } from "next/navigation";
-import React from "react";
-import prisma from "../lib/db";
-import { requireUser } from "../lib/hooks";
-import { ExternalLink, Pen, Settings, Trash, Users2 } from "lucide-react";
+import { Menu } from "lucide-react";
 
-import { EmptyState } from "../components/dashboard/EmptyState";
+import { Button } from "@/components/ui/button";
 
 import {
   DropdownMenu,
   DropdownMenuContent,
-  DropdownMenuGroup,
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { MenuActiveSwitcher } from "../components/dashboard/EventTypeSwitcher";
-import { CopyLinkMenuItem } from "../components/dashboard/CopyLinkMenuItem";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { ReactNode } from "react";
+import { requireUser } from "../lib/hooks";
+import prisma from "../lib/db";
+import { redirect } from "next/navigation";
+// import Logo from "@/public/logo.png";
+import SolerroLogo from "@/public/solerroLogo.webp";
+import Image from "next/image";
+import { DasboardLinks } from "../components/dashboard/DasboardLinks";
+import { ThemeToggle } from "../components/dashboard/ThemeToggle";
+import { Toaster } from "@/components/ui/sonner";
+import { auth, signOut } from "../lib/auth";
 
 async function getData(id: string) {
   const data = await prisma.user.findUnique({
     where: {
       id: id,
     },
-
     select: {
-      EventType: {
-        select: {
-          id: true,
-          active: true,
-          title: true,
-          url: true,
-          duration: true,
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-      },
       username: true,
+      grantId: true,
     },
   });
 
-  if (!data) {
-    return notFound();
+  if (!data?.username) {
+    return redirect("/onboarding");
+  }
+
+  if (!data.grantId) {
+    return redirect("/onboarding/grant-id");
   }
 
   return data;
 }
 
-const DashbaordPage = async () => {
-  const session = await requireUser();
-  const data = await getData(session.user?.id as string);
+export default async function Dashboard({ children }: { children: ReactNode }) {
+  const session = await auth();
+
+  if (!session?.user) {
+    return redirect("/");
+  }
+
+  const data = await getData(session.user.id as string);
 
   return (
     <>
-      <div className="flex items-center justify-between px-2">
-        <div className="sm:grid gap-1 hidden">
-          <h1 className="font-heading text-3xl md:text-4xl">Event Types</h1>
-          <p className="text-lg text-muted-foreground">
-            Create and manage your event types.
-          </p>
-        </div>
-        <Button asChild>
-          <Link href="/dashboard/new">Create New Event</Link>
-        </Button>
-      </div>
-      {data.EventType.length === 0 ? (
-        <EmptyState
-          title="You have no Event Types"
-          description="You can create your first event type by clicking the button below."
-          buttonText="Add Event Type"
-          href="/dashboard/new"
-        />
-      ) : (
-        <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
-          {data.EventType.map((item) => (
-            <div
-              className="  overflow-hidden shadow rounded-lg border relative"
-              key={item.id}
-            >
-              <div className="absolute top-2 right-2">
-                <DropdownMenu>
-                  <DropdownMenuTrigger asChild>
-                    <Button variant="outline" size="icon">
-                      <Settings className="size-4" />
-                    </Button>
-                  </DropdownMenuTrigger>
-                  <DropdownMenuContent className="w-20" align="end">
-                    <DropdownMenuLabel>Event</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuGroup>
-                      <DropdownMenuItem asChild>
-                        <Link href={`/${data.username}/${item.url}`}>
-                          <ExternalLink className="mr-2 h-4 w-4" />
-                          <span>Preview</span>
-                        </Link>
-                      </DropdownMenuItem>
-                      <CopyLinkMenuItem
-                        meetingUrl={`${process.env.NEXT_PUBLIC_URL}/${data.username}/${item.url}`}
-                      />
-                      <DropdownMenuItem asChild>
-                        <Link href={`/dashboard/event/${item.id}`}>
-                          <Pen className="mr-2 h-4 w-4" />
-                          <span>Edit</span>
-                        </Link>
-                      </DropdownMenuItem>
-                    </DropdownMenuGroup>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem asChild>
-                      <Link href={`/dashboard/event/${item.id}/delete`}>
-                        <Trash className="mr-2 h-4 w-4" />
-                        <span>Delete</span>
-                      </Link>
-                    </DropdownMenuItem>
-                  </DropdownMenuContent>
-                </DropdownMenu>
-              </div>
-
-              <Link href={`/dashboard/event/${item.id}`}>
-                <div className="p-5">
-                  <div className="flex items-center">
-                    <div className="flex-shrink-0">
-                      <Users2 className="h-6 w-6" aria-hidden="true" />
-                    </div>
-                    <div className="ml-5 w-0 flex-1">
-                      <dl>
-                        <dt className="text-sm font-medium truncate ">
-                          {item.duration} Minutes Meeting
-                        </dt>
-                        <dd>
-                          <div className="text-lg font-medium ">
-                            {item.title}
-                          </div>
-                        </dd>
-                      </dl>
-                    </div>
-                  </div>
-                </div>
+      <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+        <div className="hidden border-r bg-muted/40 md:block">
+          <div className="flex h-full max-h-screen flex-col gap-2">
+            <div className="flex h-14 items-center border-b px-4 py-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2 font-semibold py-4">
+                <Image src={SolerroLogo} alt="Logo" className="size-14" />
+                <p className="text-xl font-bold">
+                <span className="text-primary">Solerro</span>Scheduler
+                </p>
               </Link>
-              <div className="bg-muted dark:bg-gray-900 px-5 py-3 flex justify-between items-center">
-                <MenuActiveSwitcher
-                  initialChecked={item.active}
-                  eventTypeId={item.id}
-                />
-
-                <Link href={`/dashboard/event/${item.id}`}>
-                  <Button className="">Edit Event</Button>
-                </Link>
-              </div>
             </div>
-          ))}
+            <div className="flex-1">
+              <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+                <DasboardLinks />
+              </nav>
+            </div>
+          </div>
         </div>
-      )}
+        <div className="flex flex-col">
+          <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="shrink-0 md:hidden"
+                >
+                  <Menu className="h-5 w-5" />
+                  <span className="sr-only">Toggle navigation menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="flex flex-col">
+                <nav className="grid gap-2 mt-10">
+                  <DasboardLinks />
+                </nav>
+              </SheetContent>
+            </Sheet>
+
+            <div className="ml-auto flex items-center gap-x-4">
+              <ThemeToggle />
+              <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                  <Button
+                    variant="secondary"
+                    size="icon"
+                    className="rounded-full"
+                  >
+                    <Image
+                      src={session.user.image as string}
+                      alt="Profile"
+                      width={20}
+                      height={20}
+                      className="w-full h-full rounded-full"
+                    />
+                    <span className="sr-only">Toggle user menu</span>
+                  </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                  <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                  <DropdownMenuSeparator />
+                  <DropdownMenuItem asChild>
+                    <Link href="/dashboard/settings">Settings</Link>
+                  </DropdownMenuItem>
+                  <DropdownMenuItem asChild>
+                    <form
+                      className="w-full"
+                      action={async () => {
+                        "use server";
+                        await signOut();
+                      }}
+                    >
+                      <button className="w-full text-left">Log out</button>
+                    </form>
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </div>
+          </header>
+          <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+            {children}
+          </main>
+        </div>
+      </div>
+      <Toaster richColors closeButton />
     </>
   );
-};
-
-export default DashbaordPage;
+}
