1edab145520c7a4829c10e83e80346a03ca20195
Default462 <92624184+Default462@users.noreply.github.com>
2024-11-11 02:41:37 +0530

    Update Navbar.tsx

--- app/components/landingPage/Navbar.tsx
+++ app/components/landingPage/Navbar.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "@/public/logo.png";
+// import Logo from "@/public/logo.png";
+import SolerroLogo from "@/public/solerroLogo.webp";
 
 import { ThemeToggle } from "../dashboard/ThemeToggle";
 import { AuthModal } from "./AuthModal";
@@ -9,7 +10,8 @@ export function Navbar() {
     <div className="relative flex flex-col w-full py-5 mx-auto md:flex-row md:items-center md:justify-between">
       <div className="flex flex-row items-center justify-between text-sm lg:justify-start">
         <Link href="/" className="flex items-center gap-2">
-          <Image src={Logo} className="size-10" alt="Logo" />
+          {/* <Image src={Logo} className="size-10" alt="Logo" /> */}
+          <Image src={SolerroLogo} className="size-10" alt="Logo" />
 
           <h4 className="text-3xl font-semibold">
           <span className="text-primary">Solerro</span>Scheduler
