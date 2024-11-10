4a19e6027d49dea5a566786b358b22b2f948dc08
Default462 <92624184+Default462@users.noreply.github.com>
2024-11-11 00:58:45 +0530

    Update layout.tsx

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -16,7 +16,8 @@ import { ReactNode } from "react";
 import { requireUser } from "../lib/hooks";
 import prisma from "../lib/db";
 import { redirect } from "next/navigation";
-import Logo from "@/public/logo.png";
+// import Logo from "@/public/logo.png";
+import SolerroLogo from "@/public/solerroLogo.webp";
 import Image from "next/image";
 import { DasboardLinks } from "../components/dashboard/DasboardLinks";
 import { ThemeToggle } from "../components/dashboard/ThemeToggle";
@@ -59,9 +60,9 @@ export default async function Dashboard({ children }: { children: ReactNode }) {
       <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
         <div className="hidden border-r bg-muted/40 md:block">
           <div className="flex h-full max-h-screen flex-col gap-2">
-            <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-              <Link href="/" className="flex items-center gap-2 font-semibold">
-                <Image src={Logo} alt="Logo" className="size-6" />
+            <div className="flex h-14 items-center border-b px-4 py-4 lg:h-[60px] lg:px-6">
+              <Link href="/" className="flex items-center gap-2 font-semibold py-4">
+                <Image src={SolerroLogo} alt="Logo" className="size-14" />
                 <p className="text-xl font-bold">
                 <span className="text-primary">Solerro</span>Scheduler
                 </p>
