8a2f4ff2e4f6aebe8c5d1c8d3b5bd2c72248622e
Default462 <92624184+Default462@users.noreply.github.com>
2024-11-11 15:00:25 +0530

    Update nylas.ts

--- app/lib/nylas.ts
+++ app/lib/nylas.ts
@@ -2,14 +2,14 @@ import Nylas from "nylas";
 
 export const nylasConfig = {
   clientId: process.env.NYLAS_CLIENT_ID,
-  callbackUri: "https://solerro-scheduler-nylas-v2.vercel.app" + "/api/oauth/exchange",
+  callbackUri: process.env.API_BASE_URL + "/api/oauth/exchange",
   apiKey: process.env.NYLAS_API_SECRET_KEY,
   apiUri: process.env.NYLAS_API_URL,
 };
 
 const AuthConfig = {
   clientId: process.env.NYLAS_CLIENT_ID as string,
-  redirectUri: "https://solerro-scheduler-nylas-v2.vercel.app" + "/oauth/exchange",
+  redirectUri: process.env.API_BASE_URL + "/oauth/exchange",
 };
 export const nylas = new Nylas({
   apiKey: process.env.NYLAS_API_SECRET_KEY!,
